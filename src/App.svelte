8c0bb10ba65df6e99d63beaf3c67f267b8e30cf3
johnlindquist <johnlindquist@Johns-MacBook-Pro.local>
2020-05-04 17:07:48 -0600

    🎈

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,37 @@
 <script>
-  export let name
+  export let name;
+  let word = "piano";
+  let words = ["one", "two", "three"];
+
+  //return array of booleans for matching characters
+  //e.g., "yes" and "yas" returns [true, false, true]
+  let compare = function*(source, target) {
+    let i = 0;
+
+    while (i < target.length) {
+      yield { match: source[i] == target[i], item: target[i] };
+      i++;
+    }
+  };
+
+  let generateArray = function(generatorFn, ...args) {
+    return [...generatorFn(...args)];
+  };
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to
-    learn how to build Svelte apps.
-  </p>
+  <input type="text" bind:value={name} />
+  <div>{name}</div>
+
+  <h2>{word}</h2>
+  {name == word ? 'Match' : 'Keep Trying'}
+  {#each words as word}
+    <div>
+      {#each generateArray(compare, name, word) as letter}
+        <span style="color: {letter.match ? 'red' : 'black'}">
+          {letter.item}
+        </span>
+      {/each}
+    </div>
+  {/each}
 </main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
